9a56c602b6bff186d78748b09bf48db5f732d35a
junkoo44 <leonel.antonucci@gmail.com>
2025-06-30 17:11:02 -0300

    Se quita variable no utilizada en views

--- app/views.py
+++ app/views.py
@@ -76,12 +76,11 @@ def filter_by_type(request):
 # Estas funciones se usan cuando el usuario está logueado en la aplicación.
 @login_required
 def getAllFavouritesByUser(request):
-    favourite_list = []    
-    images = []
+    favourite_list = []
     favourite_list = services.getAllFavourites(request)
 
 
-    return render(request, 'favourites.html', { 'images': images, 'favourite_list': favourite_list })
+    return render(request, 'favourites.html', {'favourite_list': favourite_list })
      
 
 @login_required
